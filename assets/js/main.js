f3dc588c5581c4d64826c3f5ad630f92f37ad420
Sone Rohi Ojah <133889603+Sone-Rohi@users.noreply.github.com>
2024-03-07 16:28:41 +0200

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,54 @@
+/* ----- NAVIGATION BAR FUNCTION START ----- */
+function myMenuFunction(){
+  var menuBtn = document.getElementById("myNavMenu");
+
+  if(menuBtn.style.display === "flex"){
+    menuBtn.className = "none";
+  } 
+    else {
+      menuBtn.style.display = "flex";
+  }
+}
+/* ----- NAVIGATION BAR FUNCTION END ----- */
+
+
+/* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING START ----- */
+/* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING END ----- */   
+
+
+/* ----- TYPING EFFECT START ----- */
+/* ----- TYPING EFFECT END ----- */
+   
+
+/* ------- SCROLL REVEAL ANIMATION START ------- */
+/* ------- SCROLL REVEAL ANIMATION END ------- */
+   
+
+/* -- HOME START -- */
+/* -- HOME END-- */
+
+
+/* -- PROJECT BOX START-- */
+/* -- PROJECT BOX END-- */
+  
+
+/* -- HEADINGS START -- */
+/* -- HEADINGS END -- */
+  
+
+/* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION START -- ##  ----- */
+/* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION END -- ##  ----- */
+
+
+/* -- ABOUT INFO & CONTACT INFO START -- */
+/* -- ABOUT INFO & CONTACT INFO END -- */
+  
+
+/* -- ABOUT SKILLS & FORM BOX START -- */
+/* -- ABOUT SKILLS & FORM BOX END -- */
+  
+
+/* ----- CHANGE ACTIVE LINK START ----- */
+/* ----- CHANGE ACTIVE LINK END ----- */
+  
+   
